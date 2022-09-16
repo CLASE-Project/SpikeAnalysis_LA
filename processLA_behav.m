728344a3023282320c58bbaade71c7ea63c2fd86
John Thompson <john.a.thompson@cuanschutz.edu>
2022-09-16 16:18:20 -0600

    update files

---
+++ processLA_behav.m
@@ -0,0 +1,105 @@
+function [] = processLA_behav(subjID , NWBdir, NWBname , behDIRsave)
+
+arguments
+
+    % Required
+    subjID (1,1) string
+    % Optional
+    NWBdir (1,1) string = "NA"
+    NWBname (1,1) string = "NA"
+    behDIRsave (1,1) string = "NA"
+
+end
+
+
+% NWB Directory
+if matches(NWBdir,"NA")
+    NWBdirU = uigetdir;
+else
+    NWBdirU = NWBdir;
+end
+
+% Behavior save Directory
+if matches(behDIRsave,"NA")
+    BHsavedirU = uigetdir;
+else
+    BHsavedirU = behDIRsave;
+end
+
+% NWB file name
+if matches(NWBname,"NA")
+    NWBfname = uigetfile(NWBname);
+else
+    NWBfname = NWBname;
+end
+
+cd(NWBdirU)
+
+nwbCHECK = which('nwbtest.m');
+if isempty(nwbCHECK)
+    % find the way to bring up documents folder or search for folder
+    matNWB = uigetdir;
+    addpath(genpath(matNWB));
+end
+
+% Change directory to NWB location
+cd(NWBdirU)
+% Load NWB file
+tmpNWB_LA = nwbRead(NWBfname);
+
+% behavioral timestamps data are in microseconds
+eventStamps = tmpNWB_LA.acquisition.get('events').timestamps.load;
+eventSids = tmpNWB_LA.acquisition.get('events').data.load;
+
+% BLOCK START TTL
+% Total = 5
+% Each Trial has 5 TTLS
+% initial display of choice options % choice
+% start of response window % respWindowS
+% end of response window/response % respWindowE
+% start of outcome display % outDispS
+% end of outcome display % outDispE
+% Total of 135 Trials x 5 (5 blocks of 27 trials)
+% TRIALS = 675
+% TOTAL = 675;
+
+% Clean TTL id codes
+eventIDcs = cellstr(eventSids);
+
+posTTLinds = contains(eventIDcs,'(0x0001)');
+
+newEvts = eventStamps(posTTLinds);
+
+if length(newEvts) == 675
+     blockS = [ones(135,1) ; ones(135,1)+1 ; ones(135,1)+2 ; ones(135,1)+3;...
+               ones(135,1)+4];
+     alltrials = zeros(675,1);
+     trialepID = cell(675,1);
+     trialepNum = zeros(675,1);
+     tstrt = 1;
+     tstop = 5;
+     for ti = 1:135
+         alltrials(tstrt:tstop) = zeros(5,1) + ti;
+         trialepNum(tstrt:tstop) = 1:5;
+         trialepID(tstrt:tstop) = {'choice','respWindowS','respWindowE','outDispS',...
+             'outDispE'};
+         tstrt = tstop + 1;
+         tstop = tstop + 5;
+     end
+     triAls = alltrials;
+     triAlEvsNm = trialepNum;
+     triAlEvsId = trialepID;
+else
+    disp('MISCOUNT TTL!!!!!!!!!')
+    return
+end
+
+eventTABLE = table(blockS, triAls, triAlEvsNm, triAlEvsId, newEvts,...
+    'VariableNames',{'Blocks','Trials','TrialEvNum','TrialEvID','TrialEvTm'});
+
+cd(BHsavedirU)
+saveNAME = [char(subjID) , '_BehEvTable.mat'];
+save(saveNAME , "eventTABLE");
+
+
+end
--- test_processLA_LFP.m
+++ test_processLA_LFP.m
@@ -2,9 +2,26 @@
 
 bArea = 'amygdala';
 curDIR = 'J:';
+subI = 'clase006';
 NWBdir = [curDIR,'\01_Coding_Datasets\LossAversionPipeTest\CLASE006\NWB-data\NWB_Data'];
 BEHdir = [curDIR,'\01_Coding_Datasets\LossAversionPipeTest\CLASE006\Behavioral-data'];
 NWBname = 'MW12_Session_3_filter.nwb';
+saveLOC = [curDIR,'\01_Coding_Datasets\LossAversionPipeTest\CLASE006\NeuroPhys_Processed'];
+
+processLA_LFP(bArea, subI , NWBdir, BEHdir, NWBname,"NA","NA",saveLOC)
+
+%%
 
 
-processLA_LFP(bArea , NWBdir, BEHdir, NWBname)
+curDIR = 'J:';
+NWBdir = [curDIR,'\01_Coding_Datasets\LossAversionPipeTest\CLASE006\NWB-data\NWB_Data'];
+BEHdir = [curDIR,'\01_Coding_Datasets\LossAversionPipeTest\CLASE006\Behavioral-data'];
+NWBname = 'MW12_Session_3_filter.nwb';
+behaDIR = [curDIR ,'\01_Coding_Datasets\LossAversionPipeTest\CLASE006\Behavioral-data\EventBehavior'];
+subI = 'clase006';
+processLA_behav(subI , NWBdir, NWBname, BEHdir)
+
+
+
+%%
+
