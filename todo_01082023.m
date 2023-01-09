7ee788a911f659c8fb384de6dba3779d2943c56e
John Thompson <john.a.thompson@cuanschutz.edu>
2023-01-09 16:42:20 -0700

    update

--- todo_01082023.m
+++ todo_01082023.m
@@ -1,7 +1,17 @@
-%% 1 reprocess nwb macro
-% 1/9/23 - copy NeuroWork to Synology [nwb] & copy Synology to NeuroWork [spike]
-% 1/9/23 - copy NeuroWork to home pc [spike]
+PCname = getenv('COMPUTERNAME'); 
 
+switch PCname
+    case 'DESKTOP-95LU6PO' % JAT home pc
+        nwbLOC = 'F:\01_Coding_Datasets\LossAversionPipeTest\CLASE007\NWB-data\20220526a\NWB_Data';
+    case 'DESKTOP-I5CPDO7' % Office pc
+        nwbLOC = 'Z:\LossAversion\Patient folders\CLASE007\NWB-data\20220526a\NWB_Data';
+
+end
+
+
+
+%% 1 reprocess nwb macro
+% 1/9/23 - copy NeuroWork to home pc [spike]
 
 %% 3 final spike sort last case
 % clas19
@@ -25,7 +35,7 @@
 % CSC# / Rows: CSC 1:8 / matrix rows 1:8
 
 %% 4b. Load in NWB filtered
-nwbLOC = 'F:\01_Coding_Datasets\LossAversionPipeTest\CLASE007\NWB-data\20220526a\NWB_Data';
+% nwbLOC = 'F:\01_Coding_Datasets\LossAversionPipeTest\CLASE007\NWB-data\20220526a\NWB_Data';
 cd(nwbLOC);
 tmpLoad = nwbRead("MW13_Session_5_filter.nwb");
 eleCtable = tmpLoad.general_extracellular_ephys_electrodes.vectordata;
