c62bd20c4dd111dd728f8ff66777bc6d507dcbd8
John Thompson <john.a.thompson@cuanschutz.edu>
2023-07-18 11:23:33 -0600

    add readme for apps

---
+++ README_APPS.m
@@ -0,0 +1,11 @@
+% READ ME for APPS
+
+%% For initial wire reivew
+VisualTraceNLX_v06.mlapp
+
+
+%% For initial cluster review post OSort
+OsortSpikeAssess_v1.mlapp
+
+%% For review of clusters
+OsortOutputViewer_v2.mlapp
